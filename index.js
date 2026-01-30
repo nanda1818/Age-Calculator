ffba4dc4c2bf9c273b972f7195fbccc0ed4e4d8d
nanda1818 <snandakumar1890@gmail.com>
2026-01-30 15:48:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} years old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (month < 0 || (month === 0 && getAge() < birthdayDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+btnEl.addEventListener("click", calculateAge);
